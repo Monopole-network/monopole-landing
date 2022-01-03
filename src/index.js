5926a8fdc6c3378d74e255c16fd24c8dce978da8
SullFurix <sull.furix@gmail.com>
2022-01-03 01:16:05 +0100

    main: add index react and provider

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import { Web3ReactProvider } from '@web3-react/core';
+
+import { ethers } from 'ethers';
+
+import Layout from './layouts/Layout.js';
+
+function getLibrary(provider) {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+}
+
+ReactDOM.render(
+  <HashRouter>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </Web3ReactProvider>
+  </HashRouter>,
+  document.getElementById('root')
+);
