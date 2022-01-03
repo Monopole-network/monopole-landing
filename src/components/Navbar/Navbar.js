5e2d6d37935278dd9b5b8c67ed4ff1bbf169b40b
SullFurix <sull.furix@gmail.com>
2022-01-03 04:07:27 +0100

    navbar: update mobile view

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import {
-  Box,
-  Flex,
-  Button,
-  useColorMode,
-} from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import React from "react";
+import { Box, Flex, Button, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-import { MonopoleIconText, MonopoleIcon } from '../Icons/Icons';
+import { MonopoleIconText, MonopoleIcon } from "../Icons/Icons";
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box px="40px">
-      <Flex h="88px" alignItems="center" justifyContent="space-between">
-        <Flex display={{ sm: 'none', md: 'block', xl: 'block' }}>
-          <MonopoleIconText height="25px" width="180px" />
-        </Flex>
-        <Flex display={{ sm: 'block', md: 'none', xl: 'none' }}>
-          <MonopoleIcon height="19px" width="43px" />
+    <Box px="40px" position="fixed" left="0px" right="0px" height="88px">
+      <Flex height="100%" alignItems="center" justifyContent="space-between" >
+        <Flex>
+          <Flex display={{ sm: "none", md: "block", xl: "block" }}>
+            <MonopoleIconText height="25px" width="180px" />
+          </Flex>
+          <Flex display={{ sm: "block", md: "none", xl: "none" }}>
+            <MonopoleIcon height="19px" width="43px" />
+          </Flex>
         </Flex>
         <Flex alignItems="center">
           <Button onClick={toggleColorMode}>
-            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           </Button>
         </Flex>
       </Flex>
