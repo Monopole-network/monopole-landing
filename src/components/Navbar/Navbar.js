598b698b99db6171976a11fd233767b3539c4b18
SullFurix <sull.furix@gmail.com>
2022-01-03 01:20:05 +0100

    components: add navbar and logo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Button,
+  useColorMode,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+import { MonopoleIconText, MonopoleIcon } from '../Icons/Icons';
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Box px="40px">
+      <Flex h="88px" alignItems="center" justifyContent="space-between">
+        <Flex display={{ sm: 'none', md: 'block', xl: 'block' }}>
+          <MonopoleIconText height="25px" width="180px" />
+        </Flex>
+        <Flex display={{ sm: 'block', md: 'none', xl: 'none' }}>
+          <MonopoleIcon height="19px" width="43px" />
+        </Flex>
+        <Flex alignItems="center">
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
