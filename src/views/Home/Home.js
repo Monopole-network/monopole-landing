be8096cab22edc8cb36c1d96a0f2c5bf5fc7302d
SullFurix <sull.furix@gmail.com>
2022-01-03 01:20:33 +0100

    views: add home views

---
+++ src/views/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Chakra imports
+import { Flex, Text } from '@chakra-ui/react';
+
+function Home() {
+  return (
+    <Flex position="relative">
+      <Text>Fist Page</Text>
+    </Flex>
+  );
+}
+
+export default Home;
