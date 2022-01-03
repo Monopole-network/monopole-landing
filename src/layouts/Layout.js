e65ce07dd46ed5d9176e16876c94b3121d39cbe1
SullFurix <sull.furix@gmail.com>
2022-01-03 04:07:45 +0100

    layout: prettier

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import '@fontsource/dm-sans';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "@fontsource/dm-sans";
+import "@fontsource/montserrat";
+
 // chakra imports
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 // core components
-import UserNavbar from '../components/Navbar/Navbar.js';
+import UserNavbar from "../components/Navbar/Navbar.js";
 
-import theme from '../theme/theme.js';
+import theme from "../theme/theme.js";
 
-import Home from '../views/Home/Home.js';
+import Home from "../views/Home/Home.js";
 
 export default function Layout() {
   return (
