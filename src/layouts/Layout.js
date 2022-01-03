db84a685dc2a92e84a3abeb5cf0788c29ca33792
SullFurix <sull.furix@gmail.com>
2022-01-03 01:16:56 +0100

    layouts: add main layout (router : / )

---
+++ src/layouts/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import '@fontsource/dm-sans';
+// chakra imports
+import { ChakraProvider } from '@chakra-ui/react';
+// core components
+import UserNavbar from '../components/Navbar/Navbar.js';
+
+import theme from '../theme/theme.js';
+
+import Home from '../views/Home/Home.js';
+
+export default function Layout() {
+  return (
+    <ChakraProvider theme={theme} resetCss={false} w="100%">
+      <UserNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} key="0" />
+      </Routes>
+    </ChakraProvider>
+  );
+}
