a544b48dc7c37a27183d7590f6678b97c54db2ad
SullFurix <sull.furix@gmail.com>
2022-01-27 22:53:35 +0100

    faq: create faq component

---
+++ src/components/Faq/index.js
@@ -0,0 +1,30 @@
+const Faq = {
+  0: {
+    category: "General",
+    questions: [
+      {
+        question: "What is Gene",
+        response: "Is good",
+      },
+      {
+        question: "What is TEST",
+        response: "Is test",
+      },
+    ],
+  },
+  1: {
+    category: "Pre ICO & ICO",
+    questions: [
+      {
+        question: "What is ICO",
+        response: "Is best",
+      },
+      {
+        question: "What is esteban",
+        response: "Is strange",
+      },
+    ],
+  },
+};
+
+export { Faq };
