3b8ce62059c916caeff68dc201465770a4803c92
SullFurix <sull.furix@gmail.com>
2022-01-27 22:53:00 +0100

    team: add team component

---
+++ src/components/Team/index.js
@@ -0,0 +1,238 @@
+import justine from "../../assets/img/team/justine.png";
+import gaelle from "../../assets/img/team/gaelle.png";
+import rudy from "../../assets/img/team/rudy.png";
+import lea from "../../assets/img/team/lea.png";
+import pierre from "../../assets/img/team/pierre.png";
+import esteban from "../../assets/img/team/esteban.png";
+import kristina from "../../assets/img/team/kristina.png";
+import maxime from "../../assets/img/team/maxime.png";
+import yanis from "../../assets/img/team/yanis.png";
+import robin from "../../assets/img/team/robin.png";
+import samuel from "../../assets/img/team/samuel.png";
+import thomas from "../../assets/img/team/thomas.png";
+import thibault from "../../assets/img/team/thibault.png";
+import hugo from "../../assets/img/team/hugo.png";
+import erman from "../../assets/img/team/erman.png";
+
+import felix from "../../assets/img/advisors/felix.png";
+import elon from "../../assets/img/advisors/elon.png";
+import adrien from "../../assets/img/advisors/adrien.png";
+import anissa from "../../assets/img/advisors/anissa.png";
+import wilhem from "../../assets/img/advisors/wilhem.png";
+import julien from "../../assets/img/advisors/julien.png";
+import alexandre from "../../assets/img/advisors/alexandre.png";
+import florian from "../../assets/img/advisors/florian.png";
+
+const Team = {
+  0: {
+    name: "Justine Houge",
+    img: justine,
+    rank: "Founder & CEO",
+    link: "https://www.linkedin.com/in/justine-h-5b6155100",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/justine-h-5b6155100",
+    instagram: "",
+  },
+  1: {
+    name: "Gaëlle Vageon",
+    img: gaelle,
+    rank: "COO Real Estate",
+    link: "https://www.linkedin.com/in/gaelle-vageon/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/gaelle-vageon/",
+    instagram: "",
+  },
+  2: {
+    name: "Rudy Lemoine",
+    img: rudy,
+    rank: "COO Concierge",
+    link: "https://www.linkedin.com/in/rudy-revolife-56028914a/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/rudy-revolife-56028914a/",
+    instagram: "",
+  },
+  3: {
+    name: "Esteban Lopes",
+    img: esteban,
+    rank: "CTO",
+    link: "https://www.linkedin.com/in/esteban-lopes/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/esteban-lopes/",
+    instagram: "",
+  },
+  4: {
+    name: "Maxime Parra",
+    img: maxime,
+    rank: "Community Manager",
+    link: "https://www.linkedin.com/in/maxime-parra-069b831b3/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/maxime-parra-069b831b3/",
+    instagram: "",
+  },
+  5: {
+    name: "Léa Despinasse",
+    img: lea,
+    rank: "Sport & Health Manager",
+    link: "https://www.linkedin.com/in/l%C3%A9a-despinasse-992b73113/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/l%C3%A9a-despinasse-992b73113/",
+    instagram: "",
+  },
+  6: {
+    name: "Pierre Giannone",
+    img: pierre,
+    rank: "CMO",
+    link: "https://www.linkedin.com/in/pierre-giannone-48665411a/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/pierre-giannone-48665411a/",
+    instagram: "",
+  },
+  7: {
+    name: "Kristina Gudim",
+    img: kristina,
+    rank: "CDO",
+    link: "https://www.linkedin.com/in/kristina-gudim/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/kristina-gudim/",
+    instagram: "",
+  },
+  8: {
+    name: "Yanis Perrillat",
+    img: yanis,
+    rank: "Manager Finance",
+    link: "https://www.linkedin.com/in/yanis-perrillat-bottonet-40b149196/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/yanis-perrillat-bottonet-40b149196/",
+    instagram: "",
+  },
+  9: {
+    name: "Robin Barchechat",
+    img: robin,
+    rank: "Business Developer",
+    link: "",
+    twitter: "",
+    linkedin: "",
+    instagram: "",
+  },
+  10: {
+    name: "Samuel Adonai",
+    img: samuel,
+    rank: "Area Manager Sport & Health",
+    link: "https://www.linkedin.com/in/samuel-adonai-72895a184/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/samuel-adonai-72895a184/",
+    instagram: "",
+  },
+  11: {
+    name: "Thomas Brebion",
+    img: thomas,
+    rank: "Area Manager Sport & Health",
+    link: "https://www.linkedin.com/in/thomas-brebion-a904b3166/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/thomas-brebion-a904b3166/",
+    instagram: "",
+  },
+  12: {
+    name: "Thibault Pitolin",
+    img: thibault,
+    rank: "Real Estate Manager / Immorentier",
+    link: "https://www.instagram.com/immorentier/",
+    twitter: "",
+    linkedin: "",
+    instagram: "https://www.instagram.com/tybo.pn/",
+  },
+  13: {
+    name: "Hugo Legname",
+    img: hugo,
+    rank: "Real Estate Manager / Immorentier",
+    link: "https://www.instagram.com/immorentier/",
+    twitter: "",
+    linkedin: "",
+    instagram: "https://www.instagram.com/ugo_lgme/",
+  },
+  14: {
+    name: "Erman Choupo",
+    img: erman,
+    rank: "Support Team",
+    link: "https://www.linkedin.com/in/erman-choupo-2029b31ba/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/erman-choupo-2029b31ba/",
+    instagram: "",
+  },
+};
+
+const Advisors = {
+  0: {
+    name: "Felix Escalona",
+    img: felix,
+    rank: "Advisor South America / Spain",
+    link: "",
+    twitter: "",
+    linkedin: "",
+    instagram: "",
+  },
+  1: {
+    name: "Elon Barchechat",
+    img: elon,
+    rank: "Business Developer",
+    link: "",
+    twitter: "",
+    linkedin: "",
+    instagram: "",
+  },
+  2: {
+    name: "Adrien Vezie",
+    img: adrien,
+    rank: "Finance Advisor",
+    link: "https://www.linkedin.com/in/adrien-v%C3%A9zie-406a66116/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/adrien-v%C3%A9zie-406a66116/",
+    instagram: "",
+  },
+  3: {
+    name: "Anissa Ouadah",
+    img: anissa,
+    rank: "Legal Advisor",
+    link: "https://www.linkedin.com/in/anissa-ouadah-480ba855/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/anissa-ouadah-480ba855/",
+    instagram: "",
+  },
+  4: {
+    name: "Wilhem Faustin",
+    img: wilhem,
+    rank: "US Real Estate Advisor",
+    link: "https://www.linkedin.com/in/maxime-parra-069b831b3/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/maxime-parra-069b831b3/",
+    instagram: "",
+  },
+  5: {
+    name: "Julien Celet",
+    img: julien,
+    rank: "Strategic Advisor",
+    link: "https://www.linkedin.com/in/julien-c-35787a39/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/julien-c-35787a39/",
+    instagram: "",
+  },
+  6: {
+    name: "Alexandre Chkirate",
+    img: alexandre,
+    rank: "Communication Advisor",
+    link: "",
+    twitter: "",
+    linkedin: "",
+    instagram: "",
+  },
+  7: {
+    name: "Florian Pouvreau",
+    img: florian,
+    rank: "Marketing Advisor",
+    link: "https://www.linkedin.com/in/florianpouvreau/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/florianpouvreau/",
+    instagram: "",
+  },
+};
+export { Advisors, Team };
