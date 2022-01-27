0cc9bdd25a3947ce5d6e1aabdf64578a4162257f
SullFurix <sull.furix@gmail.com>
2022-01-27 23:00:34 +0100

    roadmap: add roadmap component

---
+++ src/components/Roadmap/index.js
@@ -0,0 +1,24 @@
+const Roadmap = [
+  {
+    title: "Q1",
+    text: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+  {
+    title: "Q2",
+    text: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+  {
+    title: "Q3",
+    text: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+  {
+    title: "Q4",
+    text: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+  {
+    title: "Q1 2023",
+    text: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+];
+
+export { Roadmap };
