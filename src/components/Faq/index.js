46ad6310402fb449ec31ebd673d1ed90fd134f4d
SullFurix <sull.furix@gmail.com>
2022-02-14 13:20:14 +0100

    faq: update faq infos

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -3,25 +3,50 @@ const Faq = {
     category: "General",
     questions: [
       {
-        question: "What is Gene",
-        response: "Is good",
+        question: "Access the MNPL platform?",
+        response: "https://monopole.network",
       },
       {
-        question: "What is TEST",
-        response: "Is test",
+        question: "Stacking For MNPL Holders?",
+        response: "Stacking implemented at ICO. Unrestricted profitability",
+      },
+      {
+        question: "Based on which Blockchain(s)?",
+        response: "The particularity and uniqueness of the MNPL project is that it is on several Blockchains: Ternoa, Avalanche, Polygon, Binance Smart Chain, Ethereum",
+      },
+      {
+        question: "How can I spend my MNPL tokens?",
+        response: "Invest: Your diversification and its returns at a glance. Film, independent media, tokenisation, NFT incubator, and many more. Over 4 verified and internationally renowned partners. Finance: Find your insurance, payment, DEX, bridge, lending and borrowing services right here. Conciergerie: Your daily life and desires offered in cryptocurrency around the world. Travel, car, activities, administrative management.",
       },
     ],
   },
   1: {
+    category: "MNPL Token",
+    questions: [
+      {
+        question: "How to buy the MNPL token?",
+        response: "By participing at the ICO",
+      },
+      {
+        question: "What is the total supply of MNPL?",
+        response: "The total supply of MNPL is: 1.000.000.000",
+      },
+      {
+        question: "What is the tokenomics of MNPL?",
+        response: "PS1: 4%, PS2: 8%, PS3: 14%, ICO: 3%, Airdrop: 2%, Liquidity providing: 13%, Founders: 13%, Advisors & Rewards: 3%, Development: 10%, Foundation: 30%",
+      },
+    ],
+  },
+  2: {
     category: "Pre ICO & ICO",
     questions: [
       {
-        question: "What is ICO",
-        response: "Is best",
+        question: "What is the date of the Presale?",
+        response: "5 february 2022",
       },
       {
-        question: "What is esteban",
-        response: "Is strange",
+        question: "What is the date of the ICO?",
+        response: "Around 5 april 2022",
       },
     ],
   },
