8011ac3fdb2520ec6bfa0433a01e36a69a71c69c
SullFurix <sull.furix@gmail.com>
2022-02-14 13:22:01 +0100

    updater: add updater onchain system with blocknumber

---
+++ src/utils/updater.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { CHAIN_INFO } from './constants';
+
+export function useUpdater() {
+  const { library, chainId } = useWeb3React();
+  const [updater, setUpdater] = useState();
+
+  useEffect(() => {
+    if (library) {
+      let stale = false;
+      library
+        .getBlockNumber()
+        .then(blockNumber => {
+          if (!stale) {
+            setUpdater(blockNumber);
+          }
+        })
+        .catch(() => {
+          if (!stale) {
+            setUpdater(null);
+          }
+        });
+
+      const updateBlockNumber = blockNumber => {
+        setUpdater(blockNumber);
+      };
+      library.on('block', updateBlockNumber);
+
+      return () => {
+        library.removeListener('block', updateBlockNumber);
+        stale = true;
+        setUpdater(undefined);
+      };
+    }
+  }, [library, chainId]);
+
+  return updater;
+}
