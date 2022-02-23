3d031bde0a517606ec7edee4903cb6297a490406
SullFurix <sull.furix@gmail.com>
2022-02-23 02:00:27 +0100

    fix: wallet connect

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -17,10 +17,10 @@ import { toHex } from "./utils";
 export function Wallet(props) {
   const { children } = props;
   // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
-  const triedEager = useEagerConnect();
+//  const triedEager = useEagerConnect();
 
   // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
-  useInactiveListener(!triedEager);
+//  useInactiveListener(!triedEager);
 
   return <>{children}</>;
 }
