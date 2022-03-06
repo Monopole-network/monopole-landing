01821c4d7a8ac6600f1008fd09f8089e25ac640d
SullFurix <sull.furix@gmail.com>
2022-03-07 00:12:19 +0100

    theme: change global scroll bar

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,20 +1,40 @@
-import { mode } from '@chakra-ui/theme-tools';
+import { mode } from "@chakra-ui/theme-tools";
 
 export const globalStyles = {
   colors: {
     gray: {
-      700: '#1f2733',
+      700: "#1f2733",
     },
-    royal: '#000126',
+    royal: "#000126",
   },
   styles: {
-    global: props => ({
+    global: (props) => ({
       body: {
-        bg: mode('gray.50', 'royal')(props),
-        fontFamily: 'DM Sans',
+        bg: mode("gray.50", "royal")(props),
+        fontFamily: "DM Sans",
+      },
+      "body::-webkit-scrollbar": {
+        "width": "18px",
+        "border-radius": "25px",
+      },
+      "body::-webkit-scrollbar-track": {
+        "box-shadow": "inset 0 0 15px 15px transparent",
+        "border": "solid 4px transparent",
+        "border-radius": "25px",
+      },
+      "body::-webkit-scrollbar-thumb": {
+        "box-shadow": "inset 0 0 15px 15px #03CB88",
+        "border": "solid 6px transparent",
+        "border-radius": "25px",
+      },
+      "body::-webkit-scrollbar-button": {
+        "display": "none",
       },
       html: {
-        fontFamily: 'DM Sans',
+        fontFamily: "DM Sans",
+        "scroll-behavior": "smooth",
+        "scrollbar-width": "thin",
+        "scrollbar-color": "#5651B7",
       },
     }),
   },
