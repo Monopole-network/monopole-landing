bded8151dff68a8afa46ee4db3de932cecbecabe
SullFurix <sull.furix@gmail.com>
2022-03-19 18:08:25 +0100

    paper: fix unused

--- src/views/Pages/Paper.js
+++ src/views/Pages/Paper.js
@@ -6,21 +6,21 @@ import {
   //  CircularProgressLabel,
   Button,
   Link,
-  useColorModeValue,
+  //useColorModeValue,
 } from "@chakra-ui/react";
 
 //import { Viewer, Worker } from "@react-pdf-viewer/core";
 
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+//import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+//import "@react-pdf-viewer/core/lib/styles/index.css";
+//import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 //import paper from "../../assets/pdf/litepaper.pdf";
 
 function Paper() {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  const color = useColorModeValue("white", "dark");
+  //  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  //const color = useColorModeValue("white", "dark");
 
   return (
     <Flex justifyContent="center" w="100%" h="100%" align="center">
