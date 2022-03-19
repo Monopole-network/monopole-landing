a58de7cac85eb5001e6ae05c6cf06dc70aa5f005
SullFurix <sull.furix@gmail.com>
2022-03-19 18:03:55 +0100

    paper: update for link

--- src/views/Pages/Paper.js
+++ src/views/Pages/Paper.js
@@ -2,27 +2,56 @@ import React from "react";
 // Chakra imports
 import {
   Flex,
-  CircularProgress,
-  CircularProgressLabel,
+  //  CircularProgress,
+  //  CircularProgressLabel,
+  Button,
+  Link,
   useColorModeValue,
 } from "@chakra-ui/react";
 
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+//import { Viewer, Worker } from "@react-pdf-viewer/core";
 
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
-import paper from "../../assets/pdf/litepaper.pdf";
+//import paper from "../../assets/pdf/litepaper.pdf";
 
 function Paper() {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const color = useColorModeValue("white", "dark");
 
   return (
-    <Flex px="8%">
-      <Worker
+    <Flex justifyContent="center" w="100%" h="100%" align="center">
+      <Link
+        href={
+          "https://github.com/Monopole-network/monopole-ui/raw/main/src/assets/pdf/litepaper.pdf"
+        }
+        h="600px"
+        w="100%"
+        isExternal
+        target="_blank"
+        _focus={{ boxShadow: "none" }}
+      >
+        <Flex justifyContent="center" w="100%" h="100%" align="center">
+          <Button
+            borderRadius="16px"
+            width="200px"
+            minWidth="160px"
+            height="64px"
+            background="#8235FF"
+            color="white"
+            _active={{
+              background: "#03CB88",
+            }}
+          >
+            Discover our project
+          </Button>
+        </Flex>
+      </Link>
+
+      {/*<Worker
         workerUrl={"https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js"}
       >
         <Viewer
@@ -35,7 +64,7 @@ function Paper() {
             </CircularProgress>
           )}
         />
-      </Worker>
+      </Worker>*/}
     </Flex>
   );
 }
