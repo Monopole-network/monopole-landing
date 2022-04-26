311d99ec359c6ff81196341cb9501874e552ca01
SullFurix <sull.furix@gmail.com>
2022-04-27 01:04:19 +0200

    social: add medium and discord

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -6,40 +6,52 @@ import {
   FaTwitch,
   FaTelegramPlane,
   FaPenAlt,
+  FaDiscord,
+  FaMedium,
 } from "react-icons/fa";
 
 const Social = {
   0: {
-    name: "Tiwtter",
+    name: "Twitter",
     logo: FaTwitter,
     link: "https://twitter.com/monopole_mnpl",
   },
   1: {
+    name: "Discord",
+    logo: FaDiscord,
+    link: "https://discord.com/invite/monopole",
+  },
+  2: {
     name: "Linkedin",
     logo: FaLinkedinIn,
     link: "https://www.linkedin.com/company/mnpl-officiel/",
   },
-  2: {
+  3: {
     name: "Instagram",
     logo: FaInstagram,
     link: "https://www.instagram.com/mnpl_off/",
   },
-  3: {
+  4: {
     name: "YouTube",
     logo: FaYoutube,
     link: "https://www.youtube.com/channel/UCjA-8ripee4FWI6eTS1ex1g",
   },
-  4: {
+  5: {
     name: "Twitch",
     logo: FaTwitch,
     link: "https://www.twitch.tv/mnpl_limited",
   },
-  5: {
+  6: {
     name: "Telegram",
     logo: FaTelegramPlane,
     link: "https://t.me/mnpl_support_bot",
   },
-  6: {
+  7: {
+    name: "Medium",
+    logo: FaMedium,
+    link: "https://monopole.medium.com/",
+  },
+  8: {
     name: "Email",
     logo: FaPenAlt,
     link:
